b004c0848b3dc5a3a4dafa45e619415d04c615cb
Wamiq Raza <sugh101@gmail.com>
2024-05-05 21:45:58 +0200

    claude example aws bedrock

---
+++ Claude/claude.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+prompt_data = """
+Act as a Shakespeare and write a poem on Generative AI
+"""
+
+# Specify the AWS region in which the service is located
+bedrock = boto3.client(service_name="bedrock-runtime", region_name="")
+
+payload = {
+    "prompt": prompt_data,
+    "maxTokens": 512,
+    "temperature": 0.8,
+    "topP": 0.8
+}
+
+body = json.dumps(payload)
+model_id = "ai21.j2-mid-v1"
+response=bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body.get("completions")[0].get("data").get("text")
+print(response_text)
