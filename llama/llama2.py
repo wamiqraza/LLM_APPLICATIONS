e22c685f50296448aae2430e8044794eac2fd5ea
Wamiq Raza <sugh101@gmail.com>
2024-05-05 21:45:02 +0200

    using aws bedrock for llama

---
+++ llama/llama2.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+prompt_data = """
+Act as a Shakespeare and write a poem on Generative AI
+"""
+
+# Specify the AWS region in which the service is located
+bedrock = boto3.client(service_name="bedrock-runtime", region_name="")
+
+payload = {
+    "prompt": "[INST]"+ prompt_data +"[INST]",
+    "max_gen_len": 512,
+    "temperature": 0.5,
+    "top_p": 0.9
+}
+
+body = json.dumps(payload)
+model_id = "meta.llama2-70b-chat-v1"
+response=bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body['generation']
+print(response_text)
