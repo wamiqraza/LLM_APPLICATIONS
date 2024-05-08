f111ed85949b754af265ae5f1e5a91fcde2857c4
Wamiq Raza <sugh101@gmail.com>
2024-05-08 11:26:27 +0200

    imports called

--- Chatgpt/chat with pdf/app.py
+++ Chatgpt/chat with pdf/app.py
@@ -1,4 +1,14 @@
 # import libraries
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import faiss
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
 # main function
 # setup openai key
 # upload file
