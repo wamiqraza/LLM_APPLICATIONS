429a4bcfee6cba840d656cdc76963a8d1ee32f17
Wamiq Raza <sugh101@gmail.com>
2024-05-08 11:31:14 +0200

    streamlit application setup

--- Chatgpt/chat with pdf/app.py
+++ Chatgpt/chat with pdf/app.py
@@ -9,10 +9,20 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-# main function
-# setup openai key
-# upload file
-# extract the text from pdf
-# split into chunks
-# create embeddings
-# show user input
+
+def main():
+    # setup openai key
+    # TODO
+    st.set_page_config(page_title="Chat with PDF", page_icon=":book")
+    st.header("Chat with PDFs ask your questions from your pdf  💬")
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # extract the text from pdf
+    # split into chunks
+    # create embeddings
+    # show user input
+
+
+if __name__ == '__main__':
+    main()
